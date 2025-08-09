34b83fa8c3d8a51773152301794d0029d42d6d98
ahmedfadlalallah <ahmed@unlimited-software.com>
2025-08-09 15:58:51 +0300

    url

--- lib/widgets/threeds.dart
+++ lib/widgets/threeds.dart
@@ -73,7 +73,7 @@ class _ThreeDSPageState extends State<ThreeDSPage> {
           },
         ),
       )
-      ..loadRequest(Uri.parse(widget.html??''));
+      ..loadHtmlString(widget.html??'');
 
   }
 
